8cb3acea708b3aeed512526459b5fd7d8da76e73
ulugbek1060 <ulugg1060@gmail.com>
2023-01-11 00:55:48 +0500

    bloc testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,103 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_block_state_managment/example_block_rest/bloc_action.dart';
+import 'package:flutter_block_state_managment/example_block_rest/person.dart';
+import 'package:flutter_block_state_managment/example_block_rest/persons_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_block_state_managment/main.dart';
+const mockedPerson1 = [
+  Person(name: 'Foo', age: 20),
+  Person(name: 'Bar', age: 30),
+];
+
+const mockedPerson2 = [
+  Person(name: 'Foo', age: 20),
+  Person(name: 'Bar', age: 30),
+];
+
+Future<Iterable<Person>> mockGetPerson1(String _) {
+  return Future.value(mockedPerson1);
+}
+
+Future<Iterable<Person>> mockGetPerson2(String _) {
+  return Future.value(mockedPerson2);
+}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group('Testing bloc', () {
+    late PersonsBloc bloc;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    setUp(() {
+      bloc = PersonsBloc();
+    });
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // bloc initial state it should be null
+    blocTest<PersonsBloc, FetchResult?>(
+      'Test initial state',
+      build: () => bloc,
+      verify: (bloc) => expect(
+        bloc.state,
+        null,
+      ),
+    );
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // fetch mock data(person1) and compare it with FetchResult.
+    blocTest(
+      'Moc retriving persons from first iterable.',
+      build: () => bloc,
+      act: (bloc) {
+        bloc.add(
+          const LoadPersonAction(
+            url: 'dummy_url_1',
+            loader: mockGetPerson1,
+          ),
+        );
+        bloc.add(
+          const LoadPersonAction(
+            url: 'dummy_url_1',
+            loader: mockGetPerson1,
+          ),
+        );
+      },
+      expect: () => [
+        const FetchResult(
+          persons: mockedPerson1,
+          isRetrivedFromCache: false,
+        ),
+        const FetchResult(
+          persons: mockedPerson1,
+          isRetrivedFromCache: true,
+        )
+      ],
+    );
+
+    // fetch mock data(person2) and compare it with FetchResult.
+    blocTest(
+      'Moc retriving persons from secnod iterable.',
+      build: () => bloc,
+      act: (bloc) {
+        bloc.add(
+          const LoadPersonAction(
+            url: 'dummy_url_2',
+            loader: mockGetPerson2,
+          ),
+        );
+        bloc.add(
+          const LoadPersonAction(
+            url: 'dummy_url_2',
+            loader: mockGetPerson2,
+          ),
+        );
+      },
+      expect: () => [
+        const FetchResult(
+          persons: mockedPerson2,
+          isRetrivedFromCache: false,
+        ),
+        const FetchResult(
+          persons: mockedPerson2,
+          isRetrivedFromCache: true,
+        )
+      ],
+    );
   });
 }
